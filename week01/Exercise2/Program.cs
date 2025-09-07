4eb7d34bbd99c10517f75f864081e644e145c290
candradehuetio <carlosandresandradehuetio@gmail.com>
2025-09-07 17:40:43 +0200

    Completion of exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter the grade percentage ");
+        string grade = Console.ReadLine();
+
+        int x = int.Parse(grade);
+        string letter = "F";
+
+        if (x >= 90)
+        {
+            letter = letter = "A";
+        }
+
+        else if (x >= 80)
+        {
+            letter = letter = "B";
+        }
+
+        else if (x >= 70)
+        {
+            letter = letter = "C";
+        }
+
+        else if (x >= 60)
+        {
+            letter = letter = "D";
+        }
+
+        else
+        {
+            letter = letter = "F";
+        }
+
+        
+
+        if (x >= 70)
+        {
+            Console.WriteLine($"Congrats! you passed the class with letter grade: {letter}");
+        }
+
+        else if (x < 70)
+        {
+            Console.WriteLine($"You have a {letter} of grade, you must have a C at least, get up and get over yourself next time!");
+        }
+
     }
 }
