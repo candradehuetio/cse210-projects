69b77dba1cff755e99d4b7f27e50c474cdadf98a
candradehuetio <carlosandresandradehuetio@gmail.com>
2025-09-07 18:36:01 +0200

    While loop with the guess input

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number?: ");
+        string x = Console.ReadLine();
+        int magicNumber = int.Parse(x);
+
+        Console.Write("What is your guess? ");
+        string y = Console.ReadLine();
+        int guessNumber = int.Parse(y);
+
+        while (guessNumber != magicNumber)
+        {
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            Console.Write("What is your guess? ");
+            string c = Console.ReadLine();
+            guessNumber = int.Parse(c);
+        }
+
+        Console.WriteLine("You guessed it!");
+
+
+
     }
 }
