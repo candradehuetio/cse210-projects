75b1cbe60d01918773ac7e545a61f8983651a705
candradehuetio <carlosandresandradehuetio@gmail.com>
2025-09-08 13:59:06 +0200

    completion core requirements exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        int number = -9999999;
+        int largestNumber = 0;
+        int sum = 0;
+
+        while (number != 0)
+        {
+
+            Console.Write("Enter number: ");
+            string x = Console.ReadLine();
+            number = int.Parse(x);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+                sum += number;
+            }
+
+            if (number > largestNumber)
+                {
+                    largestNumber = number;
+                }
+
+            if (number == 0)
+            {
+                int average = sum / numbers.Count;
+                Console.WriteLine($"The sum is: {sum}");
+                Console.WriteLine($"The average is: {average}");
+                Console.WriteLine($"The largest number is: {largestNumber}");
+
+            }
+
+        }
+
+
     }
 }
