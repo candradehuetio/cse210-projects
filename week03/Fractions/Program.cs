4971256ac156b229e0e4a99ffaee1fdaef832a5a
candradehuetio <carlosandresandradehuetio@gmail.com>
2025-09-18 13:00:07 +0200

    fractions activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        void Main()
+        {
+            Fractions fractions1 = new Fractions();
+
+            Fractions fractions2 = new Fractions(5);
+
+            Fractions fractions3 = new Fractions(3, 4);
+
+            Fractions fractions4 = new Fractions(1, 3);
+
+            double result = fractions1.GetDecimalValue();
+            Console.WriteLine(result);
+
+            result = fractions2.GetDecimalValue();
+            Console.WriteLine(result);
+
+            result = fractions3.GetDecimalValue();
+            Console.WriteLine(result);
+
+            result = fractions4.GetDecimalValue();
+            Console.WriteLine(result);
+
+
+        }
+
+        Main();
     }
 }
